800d42193cf2d7719a6c60cbeb4519941de0a251
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-09 15:18:25 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const uzdevumi = [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+      "completed": false
+    }
+  ];
+
+  const uzdevumiJSX = uzdevumi.map((uzdevums, indekss) => {
+    return (
+        <div class="uzdevumaDiv">
+          <h2>{uzdevums.title}</h2>
+          <p>ID: {uzdevums.id}</p>
+          <label>Izpildīts: </label>
+          <input type="checkbox" checked={uzdevums.completed} />
+        </div>
+    )
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {uzdevumiJSX}
+    </>
+  )
 }
 
 export default App;
